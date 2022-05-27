5e1e5b0a9d8092159ffab4f14b6190a3ff467ce3
21031099 <21031099@DESKTOP-HN72Q0P>
2022-05-27 14:05:47 +0800

    first push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,29 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21031099, 27 May 2022 12:21:36 pm
+ */
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiple(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		if(b == 0) {
+			throw new IllegalArgumentException();
+		}
+		return a / b;
+	}
+}
